96b1c4947ada52b70e8da7f7dfd2bbd9c49952f7
smileo5566 <liang40607@livemail.tw>
2017-10-02 16:05:12 +0800

    Add files via upload

---
+++ Q06.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q06 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String a = scn.nextLine();
+        char b = scn.next().charAt(0);
+        System.out.println(a.replace(b ,'\n'));
+
+        }
+
+    }
+
---
+++ Q07.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q07 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String a = scn.nextLine();
+        System.out.println(a.);
+
+    }
+}
